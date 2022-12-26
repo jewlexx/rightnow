3b84cdcae43deac270f508c63e0e0863e06cf3fd
Juliette Cordor <grassojames5@gmail.com>
2022-12-26 18:32:21 +1100

    added main logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,47 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct RightNow {
+    pub since_epoch: Duration,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl RightNow {
+    #[inline]
+    pub fn now() -> Self {
+        RightNow {
+            since_epoch: SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .expect("time went backwards"),
+        }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[inline]
+    pub fn as_secs(&self) -> u64 {
+        self.since_epoch.as_secs()
+    }
+
+    #[inline]
+    pub fn as_secs_f64(&self) -> f64 {
+        self.since_epoch.as_secs_f64()
+    }
+
+    #[inline]
+    pub fn as_secs_f32(&self) -> f32 {
+        self.since_epoch.as_secs_f32()
+    }
+
+    #[inline]
+    pub fn as_millis(&self) -> u128 {
+        self.since_epoch.as_millis()
+    }
+
+    #[inline]
+    pub fn as_micros(&self) -> u128 {
+        self.since_epoch.as_micros()
+    }
+
+    #[inline]
+    pub fn as_nanos(&self) -> u128 {
+        self.since_epoch.as_nanos()
     }
 }
